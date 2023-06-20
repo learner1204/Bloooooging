75599172dd4ad657088074883a4718b87f10e7ca
Roshan R <72537120+learner1204@users.noreply.github.com>
2023-06-20 22:33:56 +0000

    before backend

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import { Route, Routes} from 'react-router-dom';
 import Layout from './Layout';
 import IndexPage from './Pages/IndexPage';
 import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
-        <Route path={'/login'} element={<LoginPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Route>      
     </Routes>
     
